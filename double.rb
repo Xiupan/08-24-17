d8fbf3f6376fea12ec897bf9e67238f403907670
Alan Hong <xiupan@gmail.com>
2017-08-24 09:36:09 -0500

    Done in Ruby.

---
+++ double.rb
@@ -0,0 +1,12 @@
+def doubleNumbers(inputArr)
+  newArray = []
+  inputArr.each do |x|
+    newArray.push(x * 2)
+  end
+  puts newArray
+  newArray
+end
+
+arrayOne = [1,2,3,4,5]
+
+doubleNumbers(arrayOne)
